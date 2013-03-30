509e026cb6265aff45d25b9214ad0b4a9ad23c3d
Philip Youssef <pyoussef@twitter.com>
2013-03-30 11:47:55 -0700

    Cleaned up code a bit

--- src/main/scala/com/phyous/leap/SampleListener.scala
+++ src/main/scala/com/phyous/leap/SampleListener.scala
@@ -1,6 +1,6 @@
 package com.phyous.leap
 
-import java.io.IOException
+import scala.collection.JavaConverters._
 import java.lang.Math
 import com.leapmotion.leap._
 import com.leapmotion.leap.Gesture.State
@@ -47,44 +47,33 @@ class SampleListener extends Listener {
       val direction: Vector = hand.direction
       println("Hand pitch: " + Math.toDegrees(direction.pitch) + " degrees, " + "roll: " + Math.toDegrees(normal.roll) + " degrees, " + "yaw: " + Math.toDegrees(direction.yaw) + " degrees")
     }
-    val gestures: GestureList = frame.gestures()
-      var i: Int = 0
-      while (i < gestures.count) {
-          val gesture: Gesture = gestures.get(i)
-          gesture.`type` match {
-            case Gesture.Type.TYPE_CIRCLE =>
-              val circle = new CircleGesture(gesture)
-              var clockwiseness: String = null
-              if (circle.pointable.direction.angleTo(circle.normal) <= Math.PI / 4) {
-                clockwiseness = "clockwise"
-              }
-              else {
-                clockwiseness = "counterclockwise"
-              }
-              var sweptAngle: Double = 0
-              if (circle.state ne State.STATE_START) {
-                val previousUpdate = new CircleGesture(controller.frame(1).gesture(circle.id))
-                sweptAngle = (circle.progress - previousUpdate.progress) * 2 * Math.PI
-              }
-              println("Circle id: " + circle.id + ", " + circle.state + ", progress: " + circle.progress + ", radius: " + circle.radius + ", angle: " + Math.toDegrees(sweptAngle) + ", " + clockwiseness)
-            case Gesture.Type.TYPE_SWIPE =>
-              val swipe = new SwipeGesture(gesture)
-              println("Swipe id: " + swipe.id + ", " + swipe.state + ", position: " + swipe.position + ", direction: " + swipe.direction + ", speed: " + swipe.speed)
-            case Gesture.Type.TYPE_SCREEN_TAP =>
-              val screenTap = new ScreenTapGesture(gesture)
-              println("Screen Tap id: " + screenTap.id + ", " + screenTap.state + ", position: " + screenTap.position + ", direction: " + screenTap.direction)
-            case Gesture.Type.TYPE_KEY_TAP =>
-              val keyTap = new KeyTapGesture(gesture)
-              println("Key Tap id: " + keyTap.id + ", " + keyTap.state + ", position: " + keyTap.position + ", direction: " + keyTap.direction)
-            case _ =>
-              println("Unknown gesture type.")
+    for (gesture <- frame.gestures().asScala) {
+      gesture.`type` match {
+        case Gesture.Type.TYPE_CIRCLE =>
+          val circle = new CircleGesture(gesture)
+          var clockwiseness = "counterclockwise"
+          if (circle.pointable.direction.angleTo(circle.normal) <= Math.PI / 4) {
+            clockwiseness = "clockwise"
           }
-        }
-        ({
-          i += 1; i - 1
-        })
-    if (!frame.hands.empty || !gestures.empty) {
-      println
+
+          var sweptAngle: Double = 0
+          if (circle.state ne State.STATE_START) {
+            val previousUpdate = new CircleGesture(controller.frame(1).gesture(circle.id))
+            sweptAngle = (circle.progress - previousUpdate.progress) * 2 * Math.PI
+          }
+          println("Circle id: " + circle.id + ", " + circle.state + ", progress: " + circle.progress + ", radius: " + circle.radius + ", angle: " + Math.toDegrees(sweptAngle) + ", " + clockwiseness)
+        case Gesture.Type.TYPE_SWIPE =>
+          val swipe = new SwipeGesture(gesture)
+          println("Swipe id: " + swipe.id + ", " + swipe.state + ", position: " + swipe.position + ", direction: " + swipe.direction + ", speed: " + swipe.speed)
+        case Gesture.Type.TYPE_SCREEN_TAP =>
+          val screenTap = new ScreenTapGesture(gesture)
+          println("Screen Tap id: " + screenTap.id + ", " + screenTap.state + ", position: " + screenTap.position + ", direction: " + screenTap.direction)
+        case Gesture.Type.TYPE_KEY_TAP =>
+          val keyTap = new KeyTapGesture(gesture)
+          println("Key Tap id: " + keyTap.id + ", " + keyTap.state + ", position: " + keyTap.position + ", direction: " + keyTap.direction)
+        case _ =>
+          println("Unknown gesture type.")
+      }
     }
   }
 }
